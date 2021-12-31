155a4312b41c00900d73f2466292a3a3fd4a6d0c
Jeral <jeral.cris@gmail.com>
2021-12-31 14:18:52 -0600

    Formik para manejar el formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,84 @@
 import React from 'react';
+import {Formik, Form as Forms, Field, ErrorMessage} from 'formik';
 
 const Form = () => {
    return (
-      <div className="bg-gray-900 mt-10 px-5 py-10 rounded-md md:w-3/4 mx-auto">
+      <div className="bg-gray-800 mt-10 px-5 py-10 rounded-md md:w-3/4 mx-auto shadow-xl">
          <h1 className="text-gray-600 font-bold text-xl uppercase text-center">Agregar Cliente</h1>
+
+         <Formik>
+            <Forms className="mt-10">
+               <div className="mb-4">
+                  <label className="text-gray-400" htmlFor="nombre">
+                     Nombre:
+                  </label>
+                  <Field
+                     //
+                     id="nombre"
+                     type="text"
+                     className="mt-2 block w-full p-3 bg-gray-900"
+                     placeholder="Nombre del Cliente"
+                  />
+               </div>
+
+               <div className="mb-4">
+                  <label className="text-gray-400" htmlFor="empresa">
+                     Empresa:
+                  </label>
+                  <Field
+                     //
+                     id="empresa"
+                     type="text"
+                     className="mt-2 block w-full p-3 bg-gray-900"
+                     placeholder="Empresa del Cliente"
+                  />
+               </div>
+
+               <div className="mb-4">
+                  <label className="text-gray-400" htmlFor="email">
+                     E-mail:
+                  </label>
+                  <Field
+                     //
+                     id="email"
+                     type="email"
+                     className="mt-2 block w-full p-3 bg-gray-900"
+                     placeholder="Email del Cliente"
+                  />
+               </div>
+
+               <div className="mb-4">
+                  <label className="text-gray-400" htmlFor="telefono">
+                     Teléfono:
+                  </label>
+                  <Field
+                     //
+                     id="telefono"
+                     type="tel"
+                     className="mt-2 block w-full p-3 bg-gray-900"
+                     placeholder="Teléfono del Cliente"
+                  />
+               </div>
+
+               <div className="mb-4">
+                  <label className="text-gray-400" htmlFor="notas">
+                     Notas:
+                  </label>
+                  <Field
+                     //
+                     as="textarea"
+                     id="notas"
+                     type="text"
+                     className="mt-2 block w-full p-3 bg-gray-900 h-40"
+                     placeholder="Notas del Cliente"
+                  />
+               </div>
+
+               <button className="mt-5 w-full bg-gray-400 p-3 text-gray-900 uppercase font-bold text-lg rounded-md " type="submit">
+                  Agregar Cliente
+               </button>
+            </Forms>
+         </Formik>
       </div>
    );
 };
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -6,8 +6,8 @@ const Layout = () => {
    const urlActual = location.pathname;
 
    return (
-      <div className="md:flex md:min-h-screen bg-gray-800 text-white">
-         <div className="md:w-1/4 bg-gray-900  px-5 py-10">
+      <div className="md:flex md:min-h-screen bg-gray-900 text-white">
+         <div className="md:w-1/4 bg-gray-800  px-5 py-10">
             <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
             <nav className="mt-10">
                <Link
@@ -25,7 +25,7 @@ const Layout = () => {
             </nav>
          </div>
 
-         <div className="md:w-3/4 p-10">
+         <div className="md:w-3/4 p-10 md: h-screen overflow-x-auto">
             <Outlet />
          </div>
       </div>
