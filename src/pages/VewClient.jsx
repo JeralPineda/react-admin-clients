0dd00ac154a20e750897dd829a30ae067f6719e4
Jeral <jeral.cris@gmail.com>
2022-01-03 14:46:32 -0600

    Solucionando problema de carga del componente

--- src/pages/VewClient.jsx
+++ src/pages/VewClient.jsx
@@ -3,6 +3,7 @@ import {useParams} from 'react-router-dom';
 
 const VewClient = () => {
    const [cliente, setCliente] = useState({});
+   const [cargando, setCargando] = useState(true);
 
    const {id} = useParams();
 
@@ -18,43 +19,51 @@ const VewClient = () => {
          } catch (error) {
             console.log(error);
          }
+
+         setCargando(!cargando);
       };
       obtenerClienteAPI();
    }, []);
 
    return (
       <div>
-         <h1 className="font-black text-4xl">Ver Cliente: {cliente.nombre}</h1>
-         <p className="mt-3">Información del cliente</p>
+         {cargando ? (
+            'cargando....'
+         ) : (
+            <>
+               <h1 className="font-black text-4xl">Ver Cliente: {cliente.nombre}</h1>
+               <p className="mt-3">Información del cliente</p>
 
-         {cliente.nombre && (
-            <p className="text-2xl text-gray-400 mt-10">
-               <span className="text-gray-600 uppercase font-bold">Cliente:</span> {cliente.nombre}
-            </p>
-         )}
+               {cliente.nombre && (
+                  <p className="text-2xl text-gray-400 mt-10">
+                     <span className="text-gray-600 uppercase font-bold">Cliente:</span> {cliente.nombre}
+                  </p>
+               )}
 
-         {cliente.email && (
-            <p className="text-2xl text-gray-400 mt-4">
-               <span className="text-gray-600 uppercase font-bold">Email:</span> {cliente.email}
-            </p>
-         )}
+               {cliente.email && (
+                  <p className="text-2xl text-gray-400 mt-4">
+                     <span className="text-gray-600 uppercase font-bold">Email:</span> {cliente.email}
+                  </p>
+               )}
 
-         {cliente.telefono && (
-            <p className="text-2xl text-gray-400 mt-4">
-               <span className="text-gray-600 uppercase font-bold">Teléfono:</span> {cliente.telefono}
-            </p>
-         )}
+               {cliente.telefono && (
+                  <p className="text-2xl text-gray-400 mt-4">
+                     <span className="text-gray-600 uppercase font-bold">Teléfono:</span> {cliente.telefono}
+                  </p>
+               )}
 
-         {cliente.empresa && (
-            <p className="text-2xl text-gray-400 mt-4">
-               <span className="text-gray-600 uppercase font-bold">Empresa:</span> {cliente.empresa}
-            </p>
-         )}
+               {cliente.empresa && (
+                  <p className="text-2xl text-gray-400 mt-4">
+                     <span className="text-gray-600 uppercase font-bold">Empresa:</span> {cliente.empresa}
+                  </p>
+               )}
 
-         {cliente.notas && (
-            <p className="text-2xl text-gray-400 mt-4">
-               <span className="text-gray-600 uppercase font-bold">Notas:</span> {cliente.notas}
-            </p>
+               {cliente.notas && (
+                  <p className="text-2xl text-gray-400 mt-4">
+                     <span className="text-gray-600 uppercase font-bold">Notas:</span> {cliente.notas}
+                  </p>
+               )}
+            </>
          )}
       </div>
    );
